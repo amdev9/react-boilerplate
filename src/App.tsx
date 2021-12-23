aa03aa392fa567dafecce5e29b4f93af7b2562d2
Alex Matveev <amatveevdev9@gmail.com>
2021-12-24 00:06:49 +0300

    todo React testing && router && css

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import PostList from './PostsList'
 function App() {
   return (
     <div className="App">
-      <h1>React hooks demo</h1>
+      <h1>React ts saga hooks persist memo demo</h1>
       <PostList />
     </div>
   );
