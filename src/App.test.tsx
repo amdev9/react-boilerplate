aa35d8c677099353df036a0d9be90c80f698738a
Alex Matveev <amatveevdev9@gmail.com>
2021-12-25 01:14:53 +0300

    todo tests fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -10,11 +10,11 @@ import App, { LocationDisplay } from "./App";
 
 // TODO: fix tests
 
-// test("renders learn react link", () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
 
 // test("full app rendering/navigating", () => {
