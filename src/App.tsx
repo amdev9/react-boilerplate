e4d04c93d26115e69e269bceadc9ba12709ec8ab
Alex Matveev <amatveevdev9@gmail.com>
2021-12-24 03:34:23 +0300

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
-import './App.css';
-import PostList from './PostsList'
+import React from "react";
+import "./App.css";
+import {
+  Link,
+  Switch,
+  Route,
+  BrowserRouter,
+  useLocation,
+} from "react-router-dom";
+
+import Home from "./Home";
+
+const About = () => <div>You are on the about page</div>;
+const NoMatch = () => <div>No match</div>;
+
+export const LocationDisplay = () => {
+  const location = useLocation();
+
+  return <div data-testid="location-display">{location.pathname}</div>;
+};
 
 function App() {
   return (
     <div className="App">
-      <h1>React ts saga hooks persist memo demo</h1>
-      <PostList />
+      <BrowserRouter>
+        <Link to="/">Home</Link>
+
+        <Link to="/about">About</Link>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/about">
+            <About />
+          </Route>
+
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+
+        <LocationDisplay />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PostList from './PostsList'
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1>React ts saga hooks persist memo demo</h1>
+      <PostList />
+    </div>
+  );
+};
+
+export default Home;
